e52accab4a5cbdfff081eea180d739b747c22449
sriram <159551306+sriram-65@users.noreply.github.com>
2025-07-31 20:17:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let API_URL = "https://frontend-monster-iota.vercel.app"
 
-const GetButton = (buttonid , text="button") =>{
-    fetch(`${API_URL}/button`)
+const GetButton = (buttonid , text="button" , font="monospace" , fontsize="18" , classname="button" , icon=false , idname="button") =>{
+    fetch(`${API_URL}/button?classname=${classname}&font=${font}&fontSize=${fontsize}$icon=${icon}$idname=${idname}`)
     .then(res=>res.text())
     .then(data=>{
         document.getElementById(buttonid).innerHTML = data
