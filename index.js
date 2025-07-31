a5b67527b48a28c71e3feb4d1c9158cc37b71253
sriram <159551306+sriram-65@users.noreply.github.com>
2025-07-31 18:32:58 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+let API_URL = "https://frontend-monster-iota.vercel.app/"
+
+const GetButton = (buttonid) =>{
+    fetch(`${API_URL}/button`)
+    .then(res=>res.text())
+    .then(data=>document.getElementById(buttonid).innerHTML = data)
+}
